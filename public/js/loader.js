bbe8b1607751e7b2caf33c44f339244fbe112d3c
Maxime Gauthier <max45270@gmail.com>
2021-07-23 12:27:53 +0200

    add loading screen

---
+++ public/js/loader.js
@@ -0,0 +1,11 @@
+let timerLoader;
+loading();
+function loading() {
+  timerLoader = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  console.log('script loader')
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}

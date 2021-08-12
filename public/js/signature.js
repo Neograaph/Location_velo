c5185b835d055493c76528140b03f799f04fec8d
Maxime Gauthier <max45270@gmail.com>
2021-08-12 22:54:18 +0200

    clean

--- public/js/signature.js
+++ public/js/signature.js
@@ -8,17 +8,13 @@ class signature{
     this.ctx = this.canvas.getContext("2d")
     this.ctx.strokeStyle = "black"
     this.ctx.lineWidth = 2
-    // console.log(this.canvas)
-
     this.canvas.addEventListener("mousedown", (e) => {
       // je signe 
       this.sign = true
-
       // je stock mes coordonnées de départ
       this.prevX = e.clientX - this.canvas.offsetLeft
       this.prevY = e.clientY - this.canvas.offsetTop
-      // console.log(this)
-    })
+    });
     this.canvas.addEventListener("mousemove", (e) => {
       // si je signe 
       if (this.sign){
@@ -27,16 +23,15 @@ class signature{
         this.draw(this.prevX, this.prevY, currX, currY)
         this.prevX = currX
         this.prevY = currY
-      }
-    })
+      };
+    });
     this.canvas.addEventListener("mouseup", () => {
       this.sign = false
-    })
+    });
     this.canvas.addEventListener("mouseout", () => {
       this.sign = false
-    })
-    
-  }
+    });
+  };
   draw(depX, depY, destX, destY){
     this.ctx.beginPath()
     this.ctx.moveTo(depX, depY)
@@ -47,8 +42,8 @@ class signature{
   effacer(){
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
   }
-}
-
+};
+// test pour vérifier si le canvas de la signature est vide ou pas
 document.getElementById('submitBook').addEventListener('click', function() {
   const blank = isCanvasBlank(document.getElementById('signature'));
   alert(blank ? 'blank' : 'not blank');
@@ -61,15 +56,11 @@ function isCanvasBlank(canvas) {
   );
 
   return !pixelBuffer.some(color => color !== 0);
-}
-
+};
 window.onload = () => {
   let canvas = new signature("#signature")
-  // let canvasCheck = new signature("#signatureEmpty")
-  // comparer le 2eme canvas pour vérifier qu'une signature est présente ici
-  
   document.querySelector("#effacer").addEventListener("click", (e) => {
     e.preventDefault
     canvas.effacer()
   })
-}
+};

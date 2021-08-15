e2fbc19513c0f115123fadd46bce5c1ee0c5a844
Maxime Gauthier <max45270@gmail.com>
2021-08-15 19:19:26 +0200

    mergeMasterToMain

---
+++ public/js/scripthtml.js
@@ -0,0 +1,36 @@
+document.querySelector('.second-button').addEventListener('click', function () {
+    
+  document.querySelector('.animated-icon2').classList.toggle('open');
+  });
+
+  let searchBox = document.querySelectorAll('.search-box input[type="text"] + span');
+
+searchBox.forEach(elm => {
+elm.addEventListener('click', () => {
+  elm.previousElementSibling.value = '';
+});
+});
+
+$(document).ready(function(){
+
+$("a").on('click', function(event) {
+
+ 
+  if (this.hash !== "") {
+  
+    event.preventDefault();
+
+   
+    var hash = this.hash;
+
+   
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+      
+      window.location.hash = hash;
+    });
+  } 
+});
+});

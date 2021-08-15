ca2b9a8270b810396c4f9b236cfff4b9b2f3e948
Maxime Gauthier <max45270@gmail.com>
2021-08-15 04:56:17 +0200

    cleanDiapoJS

--- public/js/diapo.js
+++ public/js/diapo.js
@@ -1,82 +1,44 @@
 let i = null;
-class Slider
-{
-    constructor(auto)
-    {
+class Slider{
+    constructor(auto){
         this.auto = auto;
         this.time = null;
         this.selectedIndex = 0;
         this.slides = document.querySelector('.slides').children;
         this.total = this.slides.length;
-
-        if(this.auto)
-        {
+        if(this.auto){
             setTimeout(function(){
-
                 console.log("ceci est le timeout function il démare au bout de 1,5 secondes");
             },1500);
-            
              this.playAuto();
-  
         }
-        else
-        {
+        else{
             const controller = new SliderController();
         }
     }
-
-    playAuto()
-    {
-        
-
-        this.time = setInterval(function(){// on appelle la fonction 
-    	    
+    playAuto(){
+        this.time = setInterval(function(){
             slider.slideTheImage();
-
-        },5000);// 5000 millisecondes = 3 secondes
+        },5000);
      
     }
-
-
-    slideTheImage()
-    {
-        // let i = null
+    slideTheImage(){
         this.selectedIndex++;
 
-        if(this.selectedIndex >= this.total)
-        {
+        if(this.selectedIndex >= this.total){
             this.selectedIndex = 0;
         }
-
-        //console.log(this.selectedIndex);
-
         for (i = 0; i <  this.slides.length; i++) {
-            
-             this.slides[i].classList.remove("active");
+            this.slides[i].classList.remove("active");
         }
-
         this.slides[this.selectedIndex].classList.add("active");
-
     }
-
-    pause()
-    {
-
-      
-        console.log("pause");
-        clearInterval(this.time);// on met le temps en pause
-
-
+    pause(){
+        clearInterval(this.time);
     }
-
-    prev()
-    {
-        console.log("previous");
-         // exercise  terminer cette function afin que le slider revienne à l'image précédente et arriver à la première image reparte à la dernière
-         clearInterval(this.time);
-
-        for (i = 0; i <  this.slides.length; i++) {
-            
+    prev(){
+        clearInterval(this.time);
+        for (i = 0; i <  this.slides.length; i++) {  
         this.slides[i].classList.remove("active");
         }
     
@@ -91,15 +53,9 @@ class Slider
         console.log(this.selectedIndex)
         this.slides[this.selectedIndex].classList.add("active");
     }
-
-    next()
-    {
-        // console.log("next");
-        // exercise  terminer cette function afin que le slider passe à l'image suivante puis arriver au bout revienne à zéro
+    next(){
         clearInterval(this.time);
-
-        for (i = 0; i <  this.slides.length; i++) {
-            
+        for (i = 0; i <  this.slides.length; i++) { 
             this.slides[i].classList.remove("active");
         }
         
@@ -111,48 +67,33 @@ class Slider
         if(this.selectedIndex < 0){
             this.selectedIndex = this.total - 1;
         }
-        // console.log(this.selectedIndex)
         this.slides[this.selectedIndex].classList.add("active");
     }
 }
-
-class SliderController
-{
-    constructor()
-    {
+class SliderController{
+    constructor(){
 		this.prevButton = document.querySelector('.prev');
 		this.nextButton = document.querySelector('.next');
 		this.playButton = document.querySelector(".play");
 		this.pauseButton = document.querySelector('.pause');
         this.initController();
     }
-
-    initController()
-    {
+    initController(){
         this.prevButton.addEventListener("click", function(){
-		
-		    slider.prev();// appelle la fonction play
-
+		    slider.prev();
 	    });
 
 	    this.pauseButton.addEventListener("click", function(){
-
-    	    slider.pause();// appelle la fonction pause
-    	
+    	    slider.pause();
         });
 
         this.playButton.addEventListener("click", function(){
-		
-		    slider.playAuto();// appelle la fonction play
-
+		    slider.playAuto();
 	    });
 
 	    this.nextButton.addEventListener("click", function(){
-
-    	    slider.next();// appelle la fonction pause
-    	
+    	    slider.next();
         });
     }
 }
-
 const slider = new Slider(false);
